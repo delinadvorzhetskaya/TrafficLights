fb3372ca03efd83a6b7a908ee4882f1b28448a46
Delina Dvorzhetskaya <delina.dvorzhetskaya@gmail.com>
2021-10-19 19:40:33 +0300

    Initial Commit

---
+++ TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Дэлина Дворжецкая on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
